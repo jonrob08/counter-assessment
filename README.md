5002cac66445257570740f01ba3bad6bcab27fb0
jonrob <jonrob08@gmail.com>
2022-08-09 21:12:02 -0700

    Added code from completed project, please see unit-1-assesment repo for all commit messages

---
+++ README.md
@@ -0,0 +1,2 @@
+# Unit 1 Assessment 
+This is the finished product, please see commits/messages from this repo to see my steps https://github.com/jonrob08/unit-1-assessment
---
+++ app.js
@@ -0,0 +1,37 @@
+console.log('connected')
+
+// Global Variables
+const subtract = document.getElementById('subtract');
+const add = document.getElementById('add');
+let numberChoice = document.getElementById('numberChoice');
+let currentNum = document.getElementById('currentNumber');
+
+// Start number choice at 1
+numberChoice.value = 1
+
+console.log(numberChoice.value)
+
+// App Functionality
+// Addition
+const addition = function() {
+    sum = parseInt(numberChoice.value) + parseInt(currentNum.innerText)
+    currentNum.innerText = `${sum}`
+
+    if (sum > 0) {
+        currentNum.style.color = 'black'
+    }
+}
+
+// Subtraction
+const subtraction = function() {
+    remainder = parseInt(currentNum.innerText) - parseInt(numberChoice.value)
+    currentNum.innerText = `${remainder}`
+    
+    if (remainder < 0){
+        currentNum.style.color = 'red'
+    }
+}
+
+// Button Event Listeners
+add.addEventListener('click', addition)
+subtract.addEventListener('click', subtraction)
